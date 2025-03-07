188d1a8ffcd0a54ed082ff19f3981ee87e0cb4d2
Abhinav Aditya <abhinavaditya162@gmail.com>
2025-03-07 15:42:36 +0530

    project structure update

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ app.use(session({
 }));
 
 // Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, '../../frontend')));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Routes
 app.use('/api', routes);
