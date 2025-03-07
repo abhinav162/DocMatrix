9a5048da2fec3f1e83d64ebf670ffa16992bba67
Abhinav Aditya <abhinavaditya162@gmail.com>
2025-03-08 02:02:58 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,6 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production' ? false : false,
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000,
     path: '/',
